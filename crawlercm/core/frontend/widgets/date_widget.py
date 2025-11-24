66cf2bd8924a62a2196c1e6f21e2a013f5cddbe1
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2025-11-24 20:38:50 -0300

    [refactor] improved date and terminal widget

--- crawlercm/core/frontend/widgets/date_widget.py
+++ crawlercm/core/frontend/widgets/date_widget.py
@@ -1,11 +1,15 @@
-from PySide6 import QtWidgets
-from PySide6.QtWidgets import QFormLayout, QHBoxLayout, QLabel, QGridLayout, QVBoxLayout
+from PySide6.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QLabel,
+    QDateEdit,
+)
 from PySide6.QtCore import QDate
 from qasync import asyncSlot
 
 
-class DateSelectWidget(QtWidgets.QWidget):
-    def __init__(self, parent: QtWidgets.QWidget | None = None):
+class DateSelectWidget(QWidget):
+    def __init__(self, parent: QWidget | None = None):
         super().__init__(parent)
         date_layout = QHBoxLayout(self)
         date_layout.setContentsMargins(0, 0, 0, 0)
@@ -15,7 +19,8 @@ class DateSelectWidget(QtWidgets.QWidget):
         init_container = QHBoxLayout()
         init_container.setSpacing(15)
         self.init_date_label = QLabel("Data inicial:")
-        self.init_date = QtWidgets.QDateEdit()
+        self.init_date = QDateEdit()
+        self.init_date.setStyleSheet("font-size: 14px;")
         self.init_date.setDisplayFormat("dd/MM/yyyy")
         self.init_date.setCalendarPopup(True)
         self.init_date.setDate(QDate.currentDate())
@@ -26,7 +31,8 @@ class DateSelectWidget(QtWidgets.QWidget):
         end_container = QHBoxLayout()
         end_container.setSpacing(15)
         self.end_date_label = QLabel("Data final:  ")
-        self.end_date = QtWidgets.QDateEdit()
+        self.end_date = QDateEdit()
+        self.end_date.setStyleSheet("font-size: 14px;")
         self.end_date.setDisplayFormat("dd/MM/yyyy")
         self.end_date.setCalendarPopup(True)
         self.end_date.setDate(QDate.currentDate().addMonths(3))
--- crawlercm/core/frontend/widgets/terminal_widget.py
+++ crawlercm/core/frontend/widgets/terminal_widget.py
@@ -1,6 +1,5 @@
 # terminal_widget.py
 from PySide6.QtWidgets import QPlainTextEdit
-from PySide6.QtCore import Signal, QObject, Qt
 
 class TerminalWidget(QPlainTextEdit):
     def __init__(self, parent=None):
@@ -11,7 +10,7 @@ class TerminalWidget(QPlainTextEdit):
             QPlainTextEdit {
                 background-color: #1e1e1e;
                 color: #e0e0e0;
-                font-family: Consolas, monospace;
+                font-family: "Consolas";
                 font-size: 12px;
             }
         """)
