8716404c8cd6b19dd0f01d5cea0260305dbe1f18
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2025-11-24 15:47:01 -0300

    [refactor] improved warnings with message box and inserted developer name and software version on footer

--- crawlercm/core/frontend/widgets/tabs/configs_tab_widget.py
+++ crawlercm/core/frontend/widgets/tabs/configs_tab_widget.py
@@ -6,6 +6,7 @@ from PySide6.QtWidgets import (
     QPushButton,
     QLabel,
     QLineEdit,
+    QMessageBox,
 )
 from core.config_manager import Configs
 
@@ -14,10 +15,13 @@ class ConfigsTab(QWidget):
     def __init__(self, configs: Configs, parent: QWidget | None = None):
         super().__init__(parent)
         self.configs = configs
+        self.setContentsMargins(0, 0, 0, 0)
         self.main_layout = QVBoxLayout(self)
         self.form_layout = QFormLayout()
-        self.h_layout = QHBoxLayout()
+        self.save_btn_layout = QHBoxLayout()
+        self.footer_layout = QHBoxLayout()
 
+        # Widgets
         for key, value in self.configs.data.items():
             label = QLabel(f"{key}:".title())
             if key == "password":
@@ -27,21 +31,35 @@ class ConfigsTab(QWidget):
                 line_edit = QLineEdit(value)
 
             self.form_layout.addRow(label, line_edit)
-
+        
         save_btn = QPushButton("Salvar")
         save_btn.clicked.connect(self.save_configs)
 
-        self.h_layout.addWidget(save_btn)
+        dev = QLabel("Desenvolvido por: Rafael Costa")
+        version = QLabel("Versão: 1.0.0")
+
+
+        # Layouts
+        self.save_btn_layout.addWidget(save_btn)
+        self.footer_layout.addStretch()
+        self.footer_layout.addWidget(dev)
+        self.footer_layout.addWidget(version)
         self.main_layout.addLayout(self.form_layout)
-        self.main_layout.addLayout(self.h_layout)
+        self.main_layout.addLayout(self.save_btn_layout)
+        self.main_layout.addStretch(1)
+        self.main_layout.addLayout(self.footer_layout)
 
     def save_configs(self):
-        for row in range(self.form_layout.rowCount()):
-            label = self.form_layout.itemAt(row, QFormLayout.LabelRole)
-            field = self.form_layout.itemAt(row, QFormLayout.FieldRole)
-            if label is None or field is None:
-                continue
-            key = label.widget().text().replace(":", "").lower()
-            value = field.widget().text()
-            self.configs.set(key, value)
-        self.configs.load()
+        try:
+            for row in range(self.form_layout.rowCount()):
+                label = self.form_layout.itemAt(row, QFormLayout.LabelRole)
+                field = self.form_layout.itemAt(row, QFormLayout.FieldRole)
+                if label is None or field is None:
+                    continue
+                key = label.widget().text().replace(":", "").lower()
+                value = field.widget().text()
+                self.configs.set(key, value)
+            self.configs.load()
+            QMessageBox.information(self, "Sucesso", "Configurações salvas com sucesso")
+        except Exception as e:
+            QMessageBox.critical(self, "Erro", f"Erro ao salvar configurações: {e}")
